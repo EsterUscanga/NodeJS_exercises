c0b0a815a3daed850c7cc5d1be2853ef37d556bd
Ester Uscanga <estereuo@gmail.com>
2018-01-13 12:08:46 -0600

    Modificacion en Readme: Instrucciones de uso y propiedades de objetos

---
+++ Leccion1/objetos.js
@@ -0,0 +1,13 @@
+// Objets properties
+var obj = {}
+
+obj.nuevaPropiedad = 1
+console.log("Imprimiendo objeto")
+console.log(obj)
+
+console.log("Mostrar la propiedad del objeto")
+console.log(obj.nuevaPropiedad)
+
+delete obj.nuevaPropiedad;
+console.log("Eliminando propiedad de objeto")
+console.log(obj)
